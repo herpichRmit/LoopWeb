20cf59e4ef06e16048a5b783feb9563770a01f72
Ethan Herpich <s3935413@student.rmit.edu.au>
2023-08-27 23:20:03 +1000

    formatted sign up and in

--- loopweb/src/pages/signInPage/SignInPage.jsx
+++ loopweb/src/pages/signInPage/SignInPage.jsx
@@ -48,27 +48,29 @@ function SignInPage ({ setIsLoggedIn }) {
 
     return (
 
-        <div>
+        <div className="signIn-container">
             <form onSubmit={handleSubmit}>
-                <div className="field">
-                    <label className="label">Email Address</label>
-                    <div className="control">
-                        <input className={`input ${errors.email && 'is-danger'}`} type="email" name="email" onChange={handleChange} value={values.email || ''} required />
-                    </div>
-                    {errors.email && (
-                    <p className="help is-danger">{errors.email}</p>
-                  )}
-                </div>
-                <div className="field">
-                    <label className="label">Password</label>
-                    <div className="control">
-                        <input className={`input ${errors.password && 'is-danger'}`} type="password" name="password" onChange={handleChange} value={values.password || ''} required />
-                    </div>
-                    {errors.password && (
-                        <p className="help is-danger">{errors.password}</p>
+                <div className="signIn-display_flex">
+                    <div className="field">
+                        <label className="label">Email Address</label>
+                        <div className="control">
+                            <input className={`input ${errors.email && 'is-danger'}`} type="email" name="email" onChange={handleChange} value={values.email || ''} required />
+                        </div>
+                        {errors.email && (
+                        <p className="help is-danger">{errors.email}</p>
                     )}
-              </div>
-              <button type="submit" className="button">Login</button>
+                    </div>
+                    <div className="field">
+                        <label className="label">Password</label>
+                        <div className="control">
+                            <input className={`input ${errors.password && 'is-danger'}`} type="password" name="password" onChange={handleChange} value={values.password || ''} required />
+                        </div>
+                        {errors.password && (
+                            <p className="help is-danger">{errors.password}</p>
+                        )}
+                    </div>
+                    <button type="submit" className="button">Login</button>
+                </div>
             </form>
         </div>
     );
